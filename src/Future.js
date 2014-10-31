388f24303ec9a200efe24a5404fb403a78590fcb
chone <chonebz@gmail.com>
2014-10-31 20:28:55 +0800

    Add future implement.

--- src/Future.js
+++ src/Future.js
@@ -1,154 +1,146 @@
 (function() {
 
-	/**
-	 * @constructor
-	 * @param {Function} Optional, opt_computation.
-	 * @param {Number} Optional, opt_duration.
-	 */
-	function Future(opt_computation, opt_duration) {
-		if (opt_computation) {
-			var self = this;
-			setTimeout(function() {
-				try {
-					self._setValue(computation());
-				} catch(e) {
-					self._setError(e);
-				}
-			}, duration || 0);
-		}
+  var INCOMPLETE = 0;
+  var VALUE = 4;
+  var ERROR = 8;
+
+  function Future() {
+    this._callbacks = [];
+  };
+
+  Future.wait = function() {
+    var future = new Future();
+    var len = arguments.length;
+    var finCount = 0;
+    var results = [];
+    for (var i = 0; i < len; i++) {
+      arguments[i].then(function(value) {
+	finCount++;
+	results[index] = value;
+	if (finCount == len) {
+	  //results.push(new FutureSink(future));
+	  future._setValue(results);
 	}
+      }).catchError(function(e) {
+	future._setError(e);
+      });
+    }
+    return future;
+  };
 
-	/**
-	 * @factory
-	 * @param {Error} error.
-	 */
-	Future.error = function(error) {
-		var future = new Future();
-		future._setError(error);
-		return future;
-	};
+  Future.prototype._state = INCOMPLETE;
 
-	/**
-	 * @factory
-	 * @param {Function} computation.
-	 */
-	Future.sync = function(computation) {
-		var future = new Future();
-		try {
-			future._setValue(computation());
-		} catch(e) {
-			future._setError(e);
-		}
-		return future;
-	};
+  Future.prototype._result;
 
-	/**
-	 * @factory
-	 * @param {*} value.
-	 */
-	Future.value = function(value) {
-		var future = new Future();
+  Future.prototype._isComplete = function() {
+    return this._state >= VALUE;
+  };
+
+  Future.prototype._hasValue = function() {
+    return this._state == VALUE;
+  };
+
+  Future.prototype._hasError = function() {
+    return this._state == ERROR;
+  };
+
+  Future.prototype.then = function(fn) {
+    var future = new Future();
+    var callback = function(type, result) {
+      if (type == ERROR) {
+	future._setError(result);
+      } else {
+	try {
+	  var res = fn(result);
+	  if (res instanceof Future) {
+	    res.then(function(value) {
+	      future._setValue(value);
+	    }).catchError(function(e) {
+	      future._setError(e);
+	    });
+	  } else {
+	    if (typeof res == 'undefined') {
+	      res = result;
+	    }
+	    future._setValue(res);
+	  }
+	} catch(e) {
+	  future._setError(e);
+	}
+      }
+    };
+    this._addCallback(callback); 
+    return future;
+  };
+
+  Future.prototype.catchError = function(fn, thisObject) {
+    var future = new Future();
+    var callback = function(type, result) {
+      if (type == VALUE) {
+	future._setValue(result);
+      } else {
+	try {
+	  var res = fn(result);
+	  if(typeof res != 'undefined') {
+	    if (res instanceof Future) {
+	      res.then(function(value) {
 		future._setValue(value);
-		return future;
-	};
-
-	/**
-	 * Register callbacks to the future.
-	 * @param {Function} onValue.
-	 * @param {Function} Optional, opt_onError.
-	 * @return {Future}
-	 */
-	Future.prototype.then = function(onValue, opt_onError) {
-		if (this._hasValue) {
-			
-		} else {
-		}
-	};
-
-	/**
-	 * Handles error emitted by the future.
-	 * @param {Function} onError
-	 * @param {Function} opt_test
-	 * @return {Future}
-	 */
-	Future.prototype.catchError = function(onError, opt_test) {
-	};
-
-	/**
-	 * Timeout the future after time has passed.
-	 * @param {Number} time.
-	 * @param {Function} opt_onTime
-	 * @return {Future}
-	 */
-	Future.prototype.timeout = function(time, opt_onTime) {
-	};
-
-	/**
-	 * Register a function to be called when the future completes.
-	 * @param {Function} action.
-	 * @return {Future}
-	 */
-	Future.prototype.whenComplete = function(action) {
-	};
-
-	/**
-	 * Add value callback.
-	 * @param {Function} onValue.
-	 */
-	Future.prototype._addValueCallback = function(onValue) {
-		if (!this._valueCallbacks) {
-			this._valueCallbacks = [];
-		}
-		this._valueCallbacks.push(onValue);
-	};
-
-	/**
-	 * Fire value callback.
-	 * @param {*} value.
-	 */
-	Future.prototype._fireValueCallback = function(value) {
-		if (this._valueCallbacks) {
-			var len = this._valueCallbacks.length;
-			for(var i = 0; i < len; i++) {
-				setTimeout(function() {
-					this._valueCallbacks[i](value);
-				}, 0);
-			}
-		}
-	};
-
-	/**
-	 * Add error callback.
-	 * @param {Function} onError.
-	 */
-	Future.prototype._addErrorCallback = function(onError) {
-		if (!this._errorCallbacks) {
-			this._errorCallbacks = [];
-		}
-		this._errorCallbacks.push(onError);
-	};
-
-	/**
-	 * Complete the future with value.
-	 * @param {*} value.
-	 */
-	Future.prototype._setValue = function(value) {
-	};
-
-	/**
-	 * Complete the future as error.
-	 * @param {Error} error.
-	 */
-	Future.prototype._setError = function(error) {
-	};
-
-	// export
-	if (typeof define == 'function' && define.amd) {
-		define(function() {
-			return Future;
-		});
-	} else {
-		window.Future = Future;	
+	      }).catchError(function(e) {
+		future._setError(e);
+	      });
+	    } else {
+	      future._setValue(res);
+	    }
+	  } 
+	} catch(e) {
+	  future._setError(e);
 	}
+	
+      }
+    };
+    this._addCallback(callback); 
+    return future;
+  };
+
+  Future.prototype._addCallback = function(callback) {
+    var state = this._state;
+    var result = this._result;
+    if (state == VALUE) {
+      callback(VALUE, result);
+    } else if (state == ERROR) {
+      calllback(ERROR, result);  
+    } else {
+      this._callbacks.push(callback);
+    }
+  };
+
+  Future.prototype._setResult = function(type, result) {
+    if (this._isComplete()) throw Error('The future is complete.')
+    var callbacks = this._callbacks;
+    var len = callbacks.length;
+    for (var i = 0; i < len; i++) {
+      callbacks[i](type, result);
+    }
+    this._state = type;
+    this._result = result;
+    this._callbacks = null;
+  };
+
+  Future.prototype._setValue = function(value) {
+    this._setResult(VALUE, value);
+  };
+
+  Future.prototype._setError = function(error) {
+    this._setResult(ERROR, error);
+  };
+
+  // export
+  if (typeof define == 'function' && define.amd) {
+    define(function() {
+      return Future;
+    });
+  } else {
+    window.Future = Future;	
+  }
 
 })();
